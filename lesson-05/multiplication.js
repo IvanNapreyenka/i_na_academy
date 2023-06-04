ccee57de0a970f1130d20565642e755505ab9754
IvanNapreyenka <134631337+IvanNapreyenka@users.noreply.github.com>
2023-06-04 21:32:18 +0300

    Update multiplication.js

--- lesson-05/multiplication.js
+++ lesson-05/multiplication.js
@@ -4,8 +4,8 @@ let newStringVariable1 = 'Test';
 
 let newNumberVariable1 = 10;
 
-console.log(newStringVariable1 * newBooleanVariable1);  //string * boolean
+console.log(newStringVariable1 * newBooleanVariable1);  //string*boolean
 
-console.log(newStringVariable1 * newNumberVariable1);  //string * number
+console.log(newStringVariable1 * newNumberVariable1);  //string*number
 
-console.log(newNumberVariable1 * newBooleanVariable1);  //number * boolean
+console.log(newNumberVariable1 * newBooleanVariable1);  //number*boolean
