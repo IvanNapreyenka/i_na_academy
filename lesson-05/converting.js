79a909e3e71f8dc5b721773b424b6ac3fae7f90a
Ivan_Napreyenka <vany3737@gmail.com>
2023-06-04 21:29:11 +0300

    lesson-05

---
+++ lesson-05/converting.js
@@ -0,0 +1,13 @@
+// number, string, boolean
+
+let newConvertingNumber1 = Number(false);
+
+let newConvertingBoolean1 = Boolean(1);
+
+let newConvertingString1 = String(45);
+
+console.log(typeof newConvertingNumber1);
+
+console.log(typeof newConvertingBoolean1);
+
+console.log(typeof newConvertingString1);
