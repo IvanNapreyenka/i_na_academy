102c0f3aa7e5dd1007a226b633eb27df5ee2763e
IvanNapreyenka <134631337+IvanNapreyenka@users.noreply.github.com>
2023-06-18 21:04:39 +0300

    Update taskOn+5Promise.js

--- lesson-08/taskOn+5Promise.js
+++ lesson-08/taskOn+5Promise.js
@@ -3,15 +3,15 @@ function getRandomNumber(min, max) {
 }
 
 new Promise(function (resolve, reject) {
-    let number = getRandomNumber(1, 10);
+    let number = getRandomNumber(1, 5);
     resolve(number);
 }).then(function (number) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(number * 2), 3000);
+        setTimeout(() => resolve(Math.pow(number, 2)), 3000);
     });
 }).then(function (number) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(number * 2), 3000);
+        setTimeout(() => resolve(Math.pow(number, 2)), 3000);
     });
 
 }).then(number => console.log(number))
