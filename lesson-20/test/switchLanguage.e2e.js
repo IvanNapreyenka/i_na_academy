0a6e657da8e88f9c42ccffa03ad575cb5134c1cd
IvanNapreyenka <134631337+IvanNapreyenka@users.noreply.github.com>
2023-08-13 14:28:49 +0300

    Update switchLanguage.e2e.js

--- lesson-20/test/switchLanguage.e2e.js
+++ lesson-20/test/switchLanguage.e2e.js
@@ -2,8 +2,8 @@ const { expect } = require('chai');
 const mainPage = require ('../pageobjects/mainPage');
 const header = require('../pageobjects/components/header');
 
-describe ('Tests to check possibility to search information via header', function () {
-    it('The first search result contins the search word', async () => {
+describe ('Tests to check possibility to switch site Language', function () {
+    it('Possibility to switch language to Deutsch', async () => {
         await mainPage.navigate('https://webdriver.io/');
         await header.clickDeutschLanguage();
         expect(await header.languageInHeader.getText()).to.equal('Deutsch');
